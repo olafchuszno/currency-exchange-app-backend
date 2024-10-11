74c30f6342516d7161c3c6a73bdcd863729342a7
Olaf <olaf.chuszno@gmail.com>
2024-10-11 18:10:46 +0200

    Modify backend for Dockerfile usage

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN npm install
 
 COPY . .
 
+COPY .env .env
+
 RUN npm install -g @nestjs/cli
 
 EXPOSE 3030
--- src/transactions/entities/transaction.entity.ts
+++ src/transactions/entities/transaction.entity.ts
@@ -1,6 +1,8 @@
-export class TransactionEntity {
+export interface TransactionEntity {
+  id: number;
   transaction_eur_amount: number;
   transaction_pln_amount: number;
   currenty_exchange_rate: number;
-  timestamp: string;
+  updatedAt: string;
+  createdAt: string;
 }
